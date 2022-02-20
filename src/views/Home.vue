320820ced0c65ccd298fd3938dfcdc1d72caa007
SashaBidardel <sashabidardel@gamil.com>
2022-02-20 20:33:07 +0100

    adding exercise notas whit vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Notas />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Notas from "@/components/Notas.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Notas,
   },
 };
 </script>
